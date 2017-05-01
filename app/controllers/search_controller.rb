94695084a29c5dec680913067da3257b9cfef028
Ales Rosina <ales.rosina@gmail.com>
2017-05-01 19:25:06 +0200

    Added “no records” to views.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
 
   def index
-    @bookmarks = Bookmark.where('title like :search or url like :search or shortening like :search', search: "%#{params[:query]}%")
+    if params[:query].present?
+      @bookmarks = Bookmark.where('title like :search or url like :search or shortening like :search', search: "%#{params[:query]}%")
+    else
+      @bookmarks = []
+    end
   end
 end
