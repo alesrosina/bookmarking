e1b99219401f6eb9c55812afa05facf90275c458
Ales Rosina <ales.rosina@gmail.com>
2017-05-06 23:17:25 +0200

    Tests for tags filtering.

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -28,30 +28,39 @@ RSpec.feature 'Index page', js: true do
   end
 
   describe 'with three added bookmarks' do
-    let!(:bookmark1) { create(:bookmark, url: 'http://www.test.com/whatever', title: 'Whatever') }
-    let!(:bookmark2) { create(:bookmark, url: 'http://www.example.com/something', title: 'Something') }
+    let!(:bookmark1) { create(:bookmark, url: 'http://www.test.com/whatever', title: 'Whatever', all_tags: 'testTag, whateverTag') }
+    let!(:bookmark2) { create(:bookmark, url: 'http://www.example.com/something', title: 'Something', all_tags: 'testTag') }
     let!(:bookmark3) { create(:bookmark, url: 'http://www.example.com/other_thing', title: 'Other thing') }
+    let!(:tag1) {create(:tag, name: 'hiddenTag')}
 
-    scenario 'shows sorted bookmarks' do
+    scenario 'shows sorted bookmarks with tags' do
       visit '/'
 
-      # all added bookmarks are present on website
-      expect(page).not_to have_content 'Whatever'
-      expect(page).not_to have_content 'Something'
-      expect(page).not_to have_content 'Other thing'
-
-      # accordion is collapsed, so only titles are shown
       expect(page).to have_text 'http://www.test.com'
       expect(page).to have_text 'http://www.example.com'
-      expect(page).not_to have_text 'Whatever'
+      # only first accordion is shown
+      expect(page).to have_text 'Whatever'
+      expect(page).to have_text 'testTag'
+      expect(page).to have_text 'whateverTag'
       expect(page).not_to have_text 'Something'
       expect(page).not_to have_text 'Other thing'
 
-      # check first accordion, if it has correct items
-      click_on 'http://www.test.com'
-      expect(page).to have_text 'Whatever'
-      expect(page).not_to have_text 'Something'
-      expect(page).not_to have_text 'Other thing'
+      # open second accordion, which closes first one
+      click_on 'http://www.example.com'
+      expect(page).not_to have_text 'Whatever'
+      expect(page).to have_text 'Something'
+      expect(page).to have_text 'Other thing'
+      expect(page).to have_text 'testTag'
+      expect(page).not_to have_text 'whateverTag'
+    end
+
+    scenario 'shows only used tags in navbar' do
+      visit '/'
+      find('a', text: 'Filter by Tag').click
+
+      expect(page).to have_text 'testTag'
+      expect(page).to have_text 'whateverTag'
+      expect(page).not_to have_text 'hiddenTag'
     end
 
     scenario 'search with search form' do
